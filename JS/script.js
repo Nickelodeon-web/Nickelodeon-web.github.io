061ff2b9a18e5bad113c7ef8549c51d956e73697
Yanis santaki <161640419+Nickelodeon-web@users.noreply.github.com>
2026-03-15 22:05:26 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -38,7 +38,7 @@ const projectsData = [
         listBadge: "Linux / GLPI",
         shortDesc: "Gestion de parc informatique et Helpdesk (ITIL).",
         desc: "Mise en œuvre d'une solution de gestion d'actifs (ITAM) et de support (Helpdesk). Configuration d'un serveur LAMP, gestion des inventaires automatisés et des tickets d'incidents.",
-        image: "img/photo-glpi.avif",
+        image: "img/data-center.webp",
          githubLink: "",// Laisse vide ou mets ton lien si tu en as un
         detailsLink: "details/documentation-glpi.pdf"
     },
